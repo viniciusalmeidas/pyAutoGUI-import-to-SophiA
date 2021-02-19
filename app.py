8dfa223222bff4e821223b51e0881c7fe1435924
VINICIUS ALMEIDA DE SOUZA <v.almsou@uol.com.br>
2021-02-19 18:07:56 -0300

    pyauto

---
+++ app.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+
+# print(pg.position())
+# print(pyautogui.size()) #1920,1080
+# print(pyautogui.onScreen(12,12))
+# pg.moveRel(250,250, duration = 0.5 )
+# pg.mouseDown(button='left')
+# pg.mouseUp(button='left')
+# pg.dragTo(x2,y2,duration=1)
+# pg.scroll(1000)
+
+pg.moveTo(x=2994, y=1110, duration = 0.25 )
+pg.click(button = 'left', clicks =1, interval = 0.1)
